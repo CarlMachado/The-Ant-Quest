cffdb2d0385accc862e22502a6eda7a84b9b2cb4
Carlos Machado <carlosedumachado@edu.univali.br>
2018-11-14 23:08:20 -0200

    main

--- Project/main.cpp
+++ Project/main.cpp
@@ -9,6 +9,7 @@
  // BIBLIOTECAS
  //
 #include <stdio.h>
+#include <stdlib.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <iostream>
@@ -18,16 +19,15 @@
 #include <list>
 #include <time.h>
 #include <conio.h>
-#include <stdlib.h>
 #include <vector>
 //
 
 // CONSTANTES
 //
-#define LARGURA 1000
-#define ALTURA 640
+#define LARGURA 960
+#define ALTURA 740
 
-#define TEMPO_MAXIMO 45.0
+#define TEMPO_MAXIMO 2.0
 
 #define PAREDE 0
 #define CAMINHO 1
@@ -56,15 +56,15 @@ typedef struct Controle {
 	bool menu = false, jogar = true, pausa = false, sair = false, facil = true;
 	clock_t tempoInicial = 0, tempoFinal = 0;
 	double tempoTotal = TEMPO_MAXIMO, tempoExecucao = 0;
-	int mapaAtual = 0;
 	ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
 };
 
 typedef struct Formiga {
 	int comidaAtual = 0;
 	bool vazio = true;
-	int velocidade = 0;
-	int x = 1, y = 1;
+	int velocidade = 10;
+	int x, y;
+	int tempo = 0;
 	ALLEGRO_BITMAP *fomiga;
 };
 
@@ -74,6 +74,10 @@ typedef struct Mapa {
 	int x, y;
 	ALLEGRO_BITMAP *parede;
 	ALLEGRO_BITMAP *caminho;
+	ALLEGRO_BITMAP *background;
+	ALLEGRO_BITMAP *placa;
+	ALLEGRO_BITMAP *sombraGrande;
+	ALLEGRO_BITMAP *sombraPequena;
 };
 
 /*-----------------------------------------------------------------------------*/
@@ -107,15 +111,15 @@ void menu(Controle &c) {
 // Verifica se a formiga está ou não com comida
 void formigaAtual(Mapa &m, Formiga f) {
 	if(f.vazio)
-		m.mapa[f.x][f.y] = PERSONAGEM_VAZIO;
+		m.mapa[f.y][f.x] = PERSONAGEM_VAZIO;
 	else
-		m.mapa[f.x][f.y] = PERSONAGEM_CHEIO;
+		m.mapa[f.y][f.x] = PERSONAGEM_CHEIO;
 }
 
 // Inicializa os armazens
 void iniciarArmazem(int a[3][4]) {
-	for(int i = 0; i < 3; i++) {
-		for(int j = 0; j < 4; j++) {
+	for(size_t i = 0; i < 3; i++) {
+		for(size_t j = 0; j < 4; j++) {
 			if(i == 0) {
 				if (j == 0) {
 					a[ARMAZEM_1][POSICAO_1] = COMIDA_4;
@@ -133,12 +137,99 @@ void iniciarArmazem(int a[3][4]) {
 // Cria um novo mapa
 void novoMapa(Mapa &m) {
 	list <pair <int, int> > drillers;
+	int **map = NULL;
+	int cont = 0;
+	bool novoMapa = true;
 
-	srand(time(0));
+	srand(time(NULL));
 
-	m.x = 30 + (rand() % 20);
+	m.x = 30 + (rand() % 16);
+	if(m.x % 2 != 0)
+		m.x++;
 	m.y = (m.x / 3) * 2;
+	if(m.y % 2 != 0)
+		m.y++;
+
+	while (novoMapa) {
+		cont = 0;
+		map = new int *[m.y];
+
+		for (size_t y = 0; y < m.y; y++)
+			map[y] = new int[m.x];
+
+		for (size_t x = 0; x < m.x; x++)
+			for (size_t y = 0; y < m.y; y++)
+				map[y][x] = PAREDE;
+
+		drillers.push_back(make_pair(m.x / 2, m.y / 2));
+
+		while (drillers.size() > 0) {
+			list <pair <int, int> >::iterator n, _n, temp;
+			n = drillers.begin();
+			_n = drillers.end();
+			while (n != _n) {
+				bool remove_driller = false;
+				switch (rand() % 4)
+				{
+				case 0:
+					(*n).second -= 2;
+					if ((*n).second < 0 || map[(*n).second][(*n).first]) {
+						remove_driller = true;
+						break;
+					}
+					map[(*n).second + 1][(*n).first] = CAMINHO;
+					break;
+				case 1:
+					(*n).second += 2;
+					if ((*n).second >= m.y || map[(*n).second][(*n).first]) {
+						remove_driller = true;
+						break;
+					}
+					map[(*n).second - 1][(*n).first] = CAMINHO;
+					break;
+				case 2:
+					(*n).first -= 2;
+					if ((*n).first < 0 || map[(*n).second][(*n).first]) {
+						remove_driller = true;
+						break;
+					}
+					map[(*n).second][(*n).first + 1] = CAMINHO;
+					break;
+				case 3:
+					(*n).first += 2;
+					if ((*n).first >= m.x || map[(*n).second][(*n).first]) {
+						remove_driller = true;
+						break;
+					}
+					map[(*n).second][(*n).first - 1] = CAMINHO;
+					break;
+				}
+				if (remove_driller) {
+					n = drillers.erase(n);
+				}
+				else {
+					drillers.push_back(make_pair((*n).first, (*n).second));
+					if (rand() % 2)
+						drillers.push_back(make_pair((*n).first, (*n).second));
+					map[(*n).second][(*n).first] = CAMINHO;
+					++n;
+				}
+			}
+		}
+		for(size_t x = 0; x < m.x; x++)
+			for(size_t y = 0; y < m.y; y++)
+				if(map[y][x] == CAMINHO)
+					cont++;
+		if(cont > 200)
+			novoMapa = false;
+
+	}
+
+	m.x += 2;
+	m.y += 2;
+
 	m.mapa = new int *[m.y];
+
 	for(size_t y = 0; y < m.y; y++)
 		m.mapa[y] = new int[m.x];
 
@@ -146,62 +237,29 @@ void novoMapa(Mapa &m) {
 		for(size_t y = 0; y < m.y; y++)
 			m.mapa[y][x] = PAREDE;
 
-	drillers.push_back(make_pair(m.x / 2, m.y / 2));
+	for(size_t x = 1; x < m.x - 1; x++)
+		for(size_t y = 1; y < m.y - 1; y++)
+			m.mapa[y][x] = map[y - 1][x - 1];
 
-	while(drillers.size() > 0) {
-		list <pair <int, int> >::iterator n, _n, temp;
-		n = drillers.begin();
-		_n = drillers.end();
-		while(n != _n) {
-			bool remove_driller = false;
-			switch(rand() % 4)
-			{
-			case 0:
-				(*n).second -= 2;
-				if((*n).second < 0 || m.mapa[(*n).second][(*n).first]) {
-					remove_driller = true;
-					break;
-				}
-				m.mapa[(*n).second + 1][(*n).first] = CAMINHO;
-				break;
-			case 1:
-				(*n).second += 2;
-				if((*n).second >= m.y || m.mapa[(*n).second][(*n).first]) {
-					remove_driller = true;
-					break;
-				}
-				m.mapa[(*n).second - 1][(*n).first] = CAMINHO;
-				break;
-			case 2:
-				(*n).first -= 2;
-				if((*n).first < 0 || m.mapa[(*n).second][(*n).first]) {
-					remove_driller = true;
-					break;
-				}
-				m.mapa[(*n).second][(*n).first + 1] = CAMINHO;
-				break;
-			case 3:
-				(*n).first += 2;
-				if((*n).first >= m.x || m.mapa[(*n).second][(*n).first]) {
-					remove_driller = true;
-					break;
-				}
-				m.mapa[(*n).second][(*n).first - 1] = CAMINHO;
-				break;
-			}
-			if(remove_driller) {
-				n = drillers.erase(n);
-			} else {
-				drillers.push_back(make_pair((*n).first, (*n).second));
-				if(rand() % 2)
-					drillers.push_back(make_pair((*n).first, (*n).second));
-				m.mapa[(*n).second][(*n).first] = CAMINHO;
-				++n;
-			}
+	delete map;
+}
+
+// Colcoa uma nova formiga no mapa
+void novaFormiga(Mapa &m, Formiga &f) {
+	bool sortear = true;
+	srand(time(NULL));
+	while(sortear) {
+		f.x = rand() % m.x;
+		f.y = rand() % m.y;
+		if(m.mapa[f.y][f.x] == CAMINHO) {
+			if (f.vazio)
+				m.mapa[f.y][f.x] = PERSONAGEM_VAZIO;
+			else
+				m.mapa[f.y][f.x] = PERSONAGEM_CHEIO;
+			sortear = false;
 		}
 	}
 }
-
 // Nessa função verifica-se se vai retirar ou colocar a comida no armazém
 void verificarArmazem(Formiga &f, Mapa &m, int ARMAZEM) {
 	int LOCAL;
@@ -239,67 +297,66 @@ void verificarArmazem(Formiga &f, Mapa &m, int ARMAZEM) {
 
 // Nessa função são executados os comandos do jogo
 void lerComandos(Mapa &m, Controle &c, Formiga &f) {
-	 ALLEGRO_EVENT evento;
-	 int tecla = NADA;
-
-    al_wait_for_event(c.fila_eventos, &evento);
- 
-    if (evento.type == ALLEGRO_EVENT_KEY_DOWN){
-        switch(evento.keyboard.keycode)
-		{
-        case ALLEGRO_KEY_UP:
-            tecla = SUBIR;
-            break;
-        case ALLEGRO_KEY_DOWN:
-            tecla = DESCER;
-            break;
-        case ALLEGRO_KEY_LEFT:
-            tecla = ESQUERDA;
-            break;
-        case ALLEGRO_KEY_RIGHT:
-            tecla = DIREITA;
-            break;
-        case ALLEGRO_KEY_ESCAPE:
-            tecla = SAIR;
-			break;
-		case ALLEGRO_KEY_SPACE:
-			tecla = PEGAR;
-        }
-    }
-    else if (evento.type == ALLEGRO_EVENT_KEY_UP){
-        tecla = NADA;
-    }
-    else if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
-        c.sair = true;
-    }
-
+	int tecla = NADA;
+	while(!al_is_event_queue_empty(c.fila_eventos)){
+		ALLEGRO_EVENT evento;
+		al_wait_for_event(c.fila_eventos, &evento);
+		if(evento.type == ALLEGRO_EVENT_KEY_CHAR) {
+			switch (evento.keyboard.keycode)
+			{
+			case ALLEGRO_KEY_UP:
+				tecla = SUBIR;
+				break;
+			case ALLEGRO_KEY_DOWN:
+				tecla = DESCER;
+				break;
+			case ALLEGRO_KEY_LEFT:
+				tecla = ESQUERDA;
+				break;
+			case ALLEGRO_KEY_RIGHT:
+				tecla = DIREITA;
+				break;
+			case ALLEGRO_KEY_ESCAPE:
+				tecla = SAIR;
+				break;
+			case ALLEGRO_KEY_SPACE:
+				tecla = PEGAR;
+			}
+			f.tempo = 0;
+		} else if (evento.type == ALLEGRO_EVENT_KEY_UP) {
+			tecla = NADA;
+		} else if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			c.sair = true;
+		}
+	}
+	
 	switch(tecla)
 	{
 	case SUBIR:
-		if(m.mapa[f.x - 1][f.y] == CAMINHO) {
-			m.mapa[f.x][f.y] = CAMINHO;
-			f.x--;
-			formigaAtual(m, f);
-		}
-		break;
-	case DESCER:
-		if(m.mapa[f.x + 1][f.y] == CAMINHO) {
-			m.mapa[f.x][f.y] = CAMINHO;
-			f.x++;
-			formigaAtual(m, f);
-		}
-		break;
-	case ESQUERDA:
-		if(m.mapa[f.x][f.y - 1] == CAMINHO) {
-			m.mapa[f.x][f.y] = CAMINHO;
+		if(m.mapa[f.y - 1][f.x] == CAMINHO) {
+			m.mapa[f.y][f.x] = CAMINHO;
 			f.y--;
 			formigaAtual(m, f);
 		}
 		break;
-	case DIREITA:
-		if(m.mapa[f.x][f.y + 1] == CAMINHO) {
-			m.mapa[f.x][f.y] = CAMINHO;
+	case DESCER:
+		if(m.mapa[f.y + 1][f.x] == CAMINHO) {
+			m.mapa[f.y][f.x] = CAMINHO;
 			f.y++;
+			formigaAtual(m, f);
+		}
+		break;
+	case ESQUERDA:
+		if(m.mapa[f.y][f.x - 1] == CAMINHO) {
+			m.mapa[f.y][f.x] = CAMINHO;
+			f.x--;
+			formigaAtual(m, f);
+		}
+		break;
+	case DIREITA:
+		if(m.mapa[f.y][f.x + 1] == CAMINHO) {
+			m.mapa[f.y][f.x] = CAMINHO;
+			f.x++;
 			formigaAtual(m, f);
 		}
 		break;
@@ -309,61 +366,65 @@ void lerComandos(Mapa &m, Controle &c, Formiga &f) {
 		break;
 	case PEGAR:  // pega ou deposita comida
 	// se alguma posição ao redor da formiga for comida ela pega
-		if (m.mapa[f.x + 1][f.y] == ARMAZEM_MAPA_1 ||
-			m.mapa[f.x - 1][f.y] == ARMAZEM_MAPA_1 ||
-			m.mapa[f.x][f.y + 1] == ARMAZEM_MAPA_1 ||
-			m.mapa[f.x][f.y - 1] == ARMAZEM_MAPA_1) // armazem 1
+		if (m.mapa[f.y + 1][f.x] == ARMAZEM_MAPA_1 ||
+			m.mapa[f.y - 1][f.x] == ARMAZEM_MAPA_1 ||
+			m.mapa[f.y][f.x + 1] == ARMAZEM_MAPA_1 ||
+			m.mapa[f.y][f.x - 1] == ARMAZEM_MAPA_1) // armazem 1
 			verificarArmazem(f, m, ARMAZEM_1);
-		if (m.mapa[f.x + 1][f.y] == ARMAZEM_MAPA_2 ||
-			m.mapa[f.x - 1][f.y] == ARMAZEM_MAPA_2 ||
-			m.mapa[f.x][f.y + 1] == ARMAZEM_MAPA_2 ||
-			m.mapa[f.x][f.y - 1] == ARMAZEM_MAPA_2) // armazem 2 (meio)
+		if (m.mapa[f.y + 1][f.x] == ARMAZEM_MAPA_2 ||
+			m.mapa[f.y - 1][f.x] == ARMAZEM_MAPA_2 ||
+			m.mapa[f.y][f.x + 1] == ARMAZEM_MAPA_2 ||
+			m.mapa[f.y][f.x - 1] == ARMAZEM_MAPA_2) // armazem 2 (meio)
 			verificarArmazem(f, m, ARMAZEM_2);
-		if (m.mapa[f.x + 1][f.y] == ARMAZEM_MAPA_3 ||
-			m.mapa[f.x - 1][f.y] == ARMAZEM_MAPA_3 ||
-			m.mapa[f.x][f.y + 1] == ARMAZEM_MAPA_3 ||
-			m.mapa[f.x][f.y - 1] == ARMAZEM_MAPA_3) // armazem 3 (cima final)
+		if (m.mapa[f.y + 1][f.x] == ARMAZEM_MAPA_3 ||
+			m.mapa[f.y - 1][f.x] == ARMAZEM_MAPA_3 ||
+			m.mapa[f.y][f.x + 1] == ARMAZEM_MAPA_3 ||
+			m.mapa[f.y][f.x - 1] == ARMAZEM_MAPA_3) // armazem 3 (cima final)
 			verificarArmazem(f, m, ARMAZEM_3);
 		break;
 	}
-	al_rest(f.velocidade);
 }
 
 // Nessa função a matriz é percorrida e os números são substituidos
 void imprimir(Mapa m, Controle c, Formiga f) {
-	int x = 0, y = 0;
 	int TILE = 20;
+	int x = (LARGURA / 2) - ((m.x * TILE) / 2), y = 50 + (ALTURA / 2) - ((m.y * TILE) / 2);
 	/*---------------------------- HUD -------------------------------*/
 	//cout << "Tempo ate o terremoto: " << (int)c.tempoTotal << "   " << endl;
-
-	for(int i = 0; i < m.y; i++) {
-		for(int j = 0; j < m.x; j++) {
+	al_draw_bitmap(m.placa, 0, 0, 0);
+	/*----------------------------------------------------------------*/
+	al_draw_bitmap(m.background, 0, 100, 0);
+	for(size_t i = 0; i < m.y; i++) {
+		for(size_t j = 0; j < m.x; j++) {
 			if(m.mapa[i][j] == PAREDE) {
 				al_draw_bitmap(m.caminho, x, y, 0);
 				al_draw_bitmap(m.parede, x, y, 0);
-			} else if (m.mapa[i][j] == CAMINHO) {
+			} else if(m.mapa[i][j] == CAMINHO) {
 				al_draw_bitmap(m.caminho, x, y, 0);
-			} else if (m.mapa[i][j] == PERSONAGEM_VAZIO) {
+				al_draw_bitmap(m.caminho, x, y, 0);
+			} else if(m.mapa[i][j] == PERSONAGEM_VAZIO) {
+				al_draw_bitmap(m.caminho, x, y, 0);
+				al_draw_bitmap(f.fomiga, x, y, 0);
+			} else if(m.mapa[i][j] == PERSONAGEM_CHEIO) {
+				al_draw_bitmap(m.caminho, x, y, 0);
+				al_draw_bitmap(f.fomiga, x, y, 0);
+			} else if(m.mapa[i][j] == ARMAZEM_MAPA_1) {
 
-			} else if (m.mapa[i][j] == PERSONAGEM_CHEIO) {
+			} else if(m.mapa[i][j] == ARMAZEM_MAPA_2) {
 
-			} else if (m.mapa[i][j] == ARMAZEM_MAPA_1) {
-
-			} else if (m.mapa[i][j] == ARMAZEM_MAPA_2) {
-
-			} else if (m.mapa[i][j] == ARMAZEM_MAPA_3) {
+			} else if(m.mapa[i][j] == ARMAZEM_MAPA_3) {
 
 			}
 			x += TILE;
 		}
 		y += TILE;
-		x = 0;
+		x = (LARGURA / 2) - ((m.x * TILE) / 2);
 	}
 }
 
 // Verificam se o jogador venceu
 bool venceuDificil(int a[3][4]) {
-	for(int i = 0; i < 4; i++)
+	for(size_t i = 0; i < 4; i++)
 		if(a[ARMAZEM_3][i] == SEM_COMIDA)
 			return false;
 	return true;
@@ -397,61 +458,22 @@ bool venceuJogo(double tempo) {
 
 // PARTE DO CÓDIGO ESCRITA POR MATEUS
 //
-// Muda a posição da formiga para não ser soterrada
-void mudarPosicao(Formiga &f, Mapa &m, Controle c, int PESONAGEM) {
-	if(m.mapa[f.x][f.y] == PAREDE) {
-		m.mapa[f.x][f.y] = PESONAGEM;
-	} else {
-		for (int i = 1; i < 50; i++) {
-			if (m.mapa[f.x + i][f.y] == PAREDE) {
-				m.mapa[f.x + i][f.y] = PESONAGEM;
-				f.x++;
-			} else if (m.mapa[f.x - i][f.y] == PAREDE) {
-				m.mapa[f.x - i][f.y] = PESONAGEM;
-				f.x--;
-			} else if (m.mapa[f.x][f.y + i] == PAREDE) {
-				m.mapa[f.x][f.y + i] = PESONAGEM;
-				f.y++;
-			} else if (m.mapa[f.x][f.y - i] == PAREDE) {
-				m.mapa[f.x][f.y - i] = PESONAGEM;
-				f.y--;
-			}
-		}
-	}
-}
-//
 // função para medir tempo e sortear novo mapa
 void medirTempo(bool inicio, Mapa &m, Controle &c, Formiga &f) {
 	if (inicio) {
 		c.tempoInicial = clock();
 	} else {
-		int aux = c.mapaAtual;
-
 		c.tempoFinal = clock();
 		c.tempoTotal -= (c.tempoFinal - c.tempoInicial) / (double)CLOCKS_PER_SEC;
 		c.tempoExecucao -= (c.tempoFinal - c.tempoInicial) / (double)CLOCKS_PER_SEC;
 
 		if (c.tempoTotal < 0) {
-			for (int i = 0; i < m.x; i++) {
-				for (int j = 0; j < m.y; j++) {
-					if (m.mapa[i][j] == PERSONAGEM_VAZIO)
-						m.mapa[i][j] = CAMINHO;
-					if (m.mapa[i][j] == PERSONAGEM_CHEIO)
-						m.mapa[i][j] = CAMINHO;
-				}
-			}
 
-			srand(time(NULL));
+			novoMapa(m);
 
-			while (c.mapaAtual == aux)
-				c.mapaAtual = rand() % 3;
+			// AQUI VAI TER QUE COLOCAR NO NOVO MAPA A FORMIGA E OS ARMAZÉNS
 
 			c.tempoTotal = TEMPO_MAXIMO;
-
-			if (f.vazio)
-				mudarPosicao(f, m, c, PERSONAGEM_VAZIO);
-			else
-				mudarPosicao(f, m, c, PERSONAGEM_CHEIO);
 		}
 	}
 }
@@ -489,10 +511,15 @@ int main(void) {
 	// Carregando imagens
 	m.parede = al_load_bitmap("PAREDE.png");
 	m.caminho = al_load_bitmap("CAMINHO.png");
+	m.background = al_load_bitmap("BACKGROUND.png");
+	m.placa = al_load_bitmap("PLACA.png");
+	m.sombraGrande = al_load_bitmap("SOMBRA_GRANDE.png");
+	m.sombraPequena = al_load_bitmap("SOMBRA_PEQUENA.png");
 	f.fomiga = al_load_bitmap("FORMIGA.png");
 
 	// Inicialização dos conteúdos
 	novoMapa(m);
+	novaFormiga(m, f);
 	iniciarArmazem(m.armazem);
 
 	/*------------------------------ LOOP PRINCIPAL ---------------------------*/
@@ -504,7 +531,7 @@ int main(void) {
 		} else if (c.jogar) {
 			//medirTempo(true, m, c, f);
 			imprimir(m, c, f);
-			//lerComandos(m, c, f);
+			lerComandos(m, c, f);
 			//medirTempo(false, m, c, f);
 		} else if(c.pausa) {
 
