9fec96c63419d3407f112becd8d1f6d67f672b92
CarlMachado <carlosedumachado@users.noreply.github.com>
2018-11-15 13:39:40 -0200

    main e texturas

--- Project/main.cpp
+++ Project/main.cpp
@@ -13,7 +13,6 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <iostream>
-#include <math.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <list>
@@ -21,6 +20,7 @@
 #include <conio.h>
 #include <vector>
 //
+using namespace std;
 
 // CONSTANTES
 //
@@ -29,55 +29,102 @@
 
 #define TEMPO_MAXIMO 2.0
 
-#define PAREDE 0
-#define CAMINHO 1
-#define ARMAZEM_MAPA_1 6
-#define ARMAZEM_MAPA_2 7
-#define ARMAZEM_MAPA_3 8
-#define PERSONAGEM_VAZIO 9
-#define PERSONAGEM_CHEIO 10
-
-#define SEM_COMIDA 0
-#define COMIDA_1 1
-#define COMIDA_2 2
-#define COMIDA_3 3
-#define COMIDA_4 4
+#define QUANTIDADE_ARMAZENS 3
+#define QUANTIDADE_LOCAIS 4
 //
-
-enum armazens { ARMAZEM_1, ARMAZEM_2, ARMAZEM_3 };
-enum locais {POSICAO_1, POSICAO_2, POSICAO_3, POSICAO_4};
-enum movimentos {SUBIR, DESCER, ESQUERDA, DIREITA, PEGAR, SAIR, NADA};
-
-using namespace std;
+// ENUMS
+enum { 
+	PAREDE, 
+	CAMINHO, 
+	ITEM_PA, 
+	ITEM_BOTA, 
+	ITEM_TOCHA, 
+	ITEM_KIT, 
+	ARMAZEM_MAPA_1, 
+	ARMAZEM_MAPA_2, 
+	ARMAZEM_MAPA_3, 
+	FORMIGA_VAZIO, 
+	FORMIGA_CHEIO 
+};
+enum { 
+	ARMAZEM_1, 
+	ARMAZEM_2, 
+	ARMAZEM_3 
+};
+enum { 
+	DESMORONANDO, 
+	NORMAL 
+};
+enum { 
+	PEQUENA, 
+	GRANDE 
+};
+enum { 
+	COMIDA_1, 
+	COMIDA_2, 
+	COMIDA_3, 
+	COMIDA_4, 
+	SEM_COMIDA 
+};
+enum { 
+	POSICAO_1, 
+	POSICAO_2, 
+	POSICAO_3, 
+	POSICAO_4 
+};
+enum { 
+	SUBIR, 
+	DESCER, 
+	ESQUERDA, 
+	DIREITA, 
+	PEGAR,
+	SAIR, 
+	NADA 
+};
+//
 
 /*-------------------------------- STRUCTS ------------------------------------*/
 
 typedef struct Controle {
-	bool menu = false, jogar = true, pausa = false, sair = false, facil = true;
+	bool menu = false, jogar = true, pausa = false, sair = false;
 	clock_t tempoInicial = 0, tempoFinal = 0;
 	double tempoTotal = TEMPO_MAXIMO, tempoExecucao = 0;
 	ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
+	ALLEGRO_DISPLAY *display = NULL;
 };
 
 typedef struct Formiga {
-	int comidaAtual = 0;
-	bool vazio = true;
-	int velocidade = 10;
-	int x, y;
-	int tempo = 0;
-	ALLEGRO_BITMAP *fomiga;
+	int comidaAtual = SEM_COMIDA;
+	int x, y, DIRECAO = SUBIR;
+	float velocidade = 5.0;
+	bool vazio = false;
+	ALLEGRO_BITMAP *imgFormiga[4];
 };
 
 typedef struct Mapa {
-	int armazem[3][4];
+	int armazem[QUANTIDADE_ARMAZENS][QUANTIDADE_LOCAIS];
 	int **mapa;
 	int x, y;
-	ALLEGRO_BITMAP *parede;
-	ALLEGRO_BITMAP *caminho;
-	ALLEGRO_BITMAP *background;
-	ALLEGRO_BITMAP *placa;
-	ALLEGRO_BITMAP *sombraGrande;
-	ALLEGRO_BITMAP *sombraPequena;
+	ALLEGRO_BITMAP *imgParede;
+	ALLEGRO_BITMAP *imgCaminho;
+	ALLEGRO_BITMAP *imgBackground;
+	ALLEGRO_BITMAP *imgPlaca;
+	ALLEGRO_BITMAP *imgComida;
+	ALLEGRO_BITMAP *imgSombra[2];
+	ALLEGRO_BITMAP *imgComidaAmazem[4];
+	ALLEGRO_BITMAP *imgArmazem[2];
+};
+
+typedef struct Item {
+	int quantidadePa = 0;
+	int quantidadeBota = 0;
+	int quantidadeTocha = 0;
+	int quantidadeKit = 0;
+
+	ALLEGRO_BITMAP *imgPa;
+	ALLEGRO_BITMAP *imgBota;
+	ALLEGRO_BITMAP *imgTocha;
+	ALLEGRO_BITMAP *imgKit;
 };
 
 /*-----------------------------------------------------------------------------*/
@@ -89,13 +136,45 @@ typedef struct Mapa {
 //
 // Apresenta a tela de menu ao jogador
 void menu(Controle &c) {
-	static bool opcao = false;
-	
-	//----------------- MENU --------------------- //
+	int tecla = NADA;
+
+	while(!al_is_event_queue_empty(c.fila_eventos)) {
+		ALLEGRO_EVENT evento;
+		al_wait_for_event(c.fila_eventos, &evento);
+		if(evento.type == ALLEGRO_EVENT_KEY_DOWN) {
+			switch(evento.keyboard.keycode)
+			{
+			case ALLEGRO_KEY_UP:
+				tecla = SUBIR;
+				break;
+			case ALLEGRO_KEY_DOWN:
+				tecla = DESCER;
+				break;
+			case ALLEGRO_KEY_SPACE:
+				tecla = PEGAR;
+			}
+		} else if(evento.type == ALLEGRO_EVENT_KEY_UP) {
+			tecla = NADA;
+		} else if(evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			c.sair = true;
+		}
+	}
+
+	switch (tecla)
+	{
+	case SUBIR:
+
+		break;
+	case DESCER:
+
+		break;
+	case PEGAR:
+
+		break;
+	}
 
 	//LÓGICA MENU
 
-	//-------------------------------------------- //
 }
 //
 // FIM DA PARTE DE CÓDIGO ESCRITA POR CAIO
@@ -111,24 +190,24 @@ void menu(Controle &c) {
 // Verifica se a formiga está ou não com comida
 void formigaAtual(Mapa &m, Formiga f) {
 	if(f.vazio)
-		m.mapa[f.y][f.x] = PERSONAGEM_VAZIO;
+		m.mapa[f.y][f.x] = FORMIGA_VAZIO;
 	else
-		m.mapa[f.y][f.x] = PERSONAGEM_CHEIO;
+		m.mapa[f.y][f.x] = FORMIGA_CHEIO;
 }
 
 // Inicializa os armazens
-void iniciarArmazem(int a[3][4]) {
-	for(size_t i = 0; i < 3; i++) {
-		for(size_t j = 0; j < 4; j++) {
-			if(i == 0) {
-				if (j == 0) {
-					a[ARMAZEM_1][POSICAO_1] = COMIDA_4;
-					a[ARMAZEM_1][POSICAO_2] = COMIDA_3;
-					a[ARMAZEM_1][POSICAO_3] = COMIDA_2;
-					a[ARMAZEM_1][POSICAO_3] = COMIDA_1;
+void iniciarArmazem(int armazem[QUANTIDADE_ARMAZENS][QUANTIDADE_LOCAIS]) {
+	for(size_t ARMAZEM = 0; ARMAZEM < QUANTIDADE_ARMAZENS; ARMAZEM++) {
+		for(size_t LOCAL = 0; LOCAL < QUANTIDADE_LOCAIS; LOCAL++) {
+			if(ARMAZEM == 0) {
+				if (LOCAL == 0) {
+					armazem[ARMAZEM_1][POSICAO_1] = COMIDA_4;
+					armazem[ARMAZEM_1][POSICAO_2] = COMIDA_3;
+					armazem[ARMAZEM_1][POSICAO_3] = COMIDA_2;
+					armazem[ARMAZEM_1][POSICAO_3] = COMIDA_1;
 				}
 			} else {
-				a[i][j] = SEM_COMIDA;
+				armazem[ARMAZEM][LOCAL] = SEM_COMIDA;
 			}
 		}
 	}
@@ -245,7 +324,7 @@ void novoMapa(Mapa &m) {
 }
 
 // Colcoa uma nova formiga no mapa
-void novaFormiga(Mapa &m, Formiga &f) {
+void posicionarFormiga(Mapa &m, Formiga &f) {
 	bool sortear = true;
 	srand(time(NULL));
 	while(sortear) {
@@ -253,9 +332,9 @@ void novaFormiga(Mapa &m, Formiga &f) {
 		f.y = rand() % m.y;
 		if(m.mapa[f.y][f.x] == CAMINHO) {
 			if (f.vazio)
-				m.mapa[f.y][f.x] = PERSONAGEM_VAZIO;
+				m.mapa[f.y][f.x] = FORMIGA_VAZIO;
 			else
-				m.mapa[f.y][f.x] = PERSONAGEM_CHEIO;
+				m.mapa[f.y][f.x] = FORMIGA_CHEIO;
 			sortear = false;
 		}
 	}
@@ -265,17 +344,17 @@ void verificarArmazem(Formiga &f, Mapa &m, int ARMAZEM) {
 	int LOCAL;
 	bool retirar = false;
 
-	if(f.comidaAtual == 0) { // se a formiga não tiver comida, significa que ela vai pegar do armazém
+	if(f.comidaAtual == 0) { // Se a formiga não tiver segurando comida, significa que ela vai pegar do armazém
 		for (LOCAL = 3; LOCAL >= 0; LOCAL--) {
 			if (m.armazem[ARMAZEM][LOCAL] != SEM_COMIDA) {
 				f.comidaAtual = m.armazem[ARMAZEM][LOCAL];
 				m.armazem[ARMAZEM][LOCAL] = SEM_COMIDA;
-				m.mapa[f.x][f.y] = PERSONAGEM_CHEIO;
+				m.mapa[f.x][f.y] = FORMIGA_CHEIO;
 				f.vazio = false;
 				LOCAL = -1;
 			}
 		}
-	} else { // se já tiver ela vai colocar no armazém
+	} else { // Se já tiver, ela vai colocar no armazém
 		for(LOCAL = 0; LOCAL <= 3; LOCAL++) {
 			if(m.armazem[ARMAZEM][LOCAL] == SEM_COMIDA) {
 				if(LOCAL == 0)
@@ -286,7 +365,7 @@ void verificarArmazem(Formiga &f, Mapa &m, int ARMAZEM) {
 				if(retirar) {
 					m.armazem[ARMAZEM][LOCAL] = f.comidaAtual;
 					f.comidaAtual = SEM_COMIDA;
-					m.mapa[f.x][f.y] = PERSONAGEM_VAZIO;
+					m.mapa[f.x][f.y] = FORMIGA_VAZIO;
 					f.vazio = true;
 				}
 				LOCAL = 4;
@@ -306,15 +385,19 @@ void lerComandos(Mapa &m, Controle &c, Formiga &f) {
 			{
 			case ALLEGRO_KEY_UP:
 				tecla = SUBIR;
+				f.DIRECAO = SUBIR;
 				break;
 			case ALLEGRO_KEY_DOWN:
 				tecla = DESCER;
+				f.DIRECAO = DESCER;
 				break;
 			case ALLEGRO_KEY_LEFT:
 				tecla = ESQUERDA;
+				f.DIRECAO = ESQUERDA;
 				break;
 			case ALLEGRO_KEY_RIGHT:
 				tecla = DIREITA;
+				f.DIRECAO = DIREITA;
 				break;
 			case ALLEGRO_KEY_ESCAPE:
 				tecla = SAIR;
@@ -322,7 +405,6 @@ void lerComandos(Mapa &m, Controle &c, Formiga &f) {
 			case ALLEGRO_KEY_SPACE:
 				tecla = PEGAR;
 			}
-			f.tempo = 0;
 		} else if (evento.type == ALLEGRO_EVENT_KEY_UP) {
 			tecla = NADA;
 		} else if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
@@ -364,56 +446,67 @@ void lerComandos(Mapa &m, Controle &c, Formiga &f) {
 		c.pausa = true;
 		c.jogar = false;
 		break;
-	case PEGAR:  // pega ou deposita comida
-	// se alguma posição ao redor da formiga for comida ela pega
+	case PEGAR:
+		// Pega ou deposita comida
+		// Se alguma posição ao redor da formiga for armazém ela chama a função verificar armazém
 		if (m.mapa[f.y + 1][f.x] == ARMAZEM_MAPA_1 ||
 			m.mapa[f.y - 1][f.x] == ARMAZEM_MAPA_1 ||
 			m.mapa[f.y][f.x + 1] == ARMAZEM_MAPA_1 ||
-			m.mapa[f.y][f.x - 1] == ARMAZEM_MAPA_1) // armazem 1
+			m.mapa[f.y][f.x - 1] == ARMAZEM_MAPA_1)		// O ARMAZÉM 1 É O INICIAL (DESMORONANDO)
 			verificarArmazem(f, m, ARMAZEM_1);
 		if (m.mapa[f.y + 1][f.x] == ARMAZEM_MAPA_2 ||
 			m.mapa[f.y - 1][f.x] == ARMAZEM_MAPA_2 ||
 			m.mapa[f.y][f.x + 1] == ARMAZEM_MAPA_2 ||
-			m.mapa[f.y][f.x - 1] == ARMAZEM_MAPA_2) // armazem 2 (meio)
+			m.mapa[f.y][f.x - 1] == ARMAZEM_MAPA_2)
 			verificarArmazem(f, m, ARMAZEM_2);
 		if (m.mapa[f.y + 1][f.x] == ARMAZEM_MAPA_3 ||
 			m.mapa[f.y - 1][f.x] == ARMAZEM_MAPA_3 ||
 			m.mapa[f.y][f.x + 1] == ARMAZEM_MAPA_3 ||
-			m.mapa[f.y][f.x - 1] == ARMAZEM_MAPA_3) // armazem 3 (cima final)
+			m.mapa[f.y][f.x - 1] == ARMAZEM_MAPA_3)
 			verificarArmazem(f, m, ARMAZEM_3);
 		break;
 	}
 }
 
 // Nessa função a matriz é percorrida e os números são substituidos
-void imprimir(Mapa m, Controle c, Formiga f) {
-	int TILE = 20;
+void desenharFrame(Mapa m, Controle c, Formiga f) {
+	const int TILE = 20;
+	const int PLACAR = 100;
 	int x = (LARGURA / 2) - ((m.x * TILE) / 2), y = 50 + (ALTURA / 2) - ((m.y * TILE) / 2);
 	/*---------------------------- HUD -------------------------------*/
 	//cout << "Tempo ate o terremoto: " << (int)c.tempoTotal << "   " << endl;
-	al_draw_bitmap(m.placa, 0, 0, 0);
+	al_draw_bitmap(m.imgPlaca, 0, 0, NULL);
 	/*----------------------------------------------------------------*/
-	al_draw_bitmap(m.background, 0, 100, 0);
+	al_draw_bitmap(m.imgBackground, 0, PLACAR, NULL);
 	for(size_t i = 0; i < m.y; i++) {
 		for(size_t j = 0; j < m.x; j++) {
 			if(m.mapa[i][j] == PAREDE) {
-				al_draw_bitmap(m.caminho, x, y, 0);
-				al_draw_bitmap(m.parede, x, y, 0);
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+				al_draw_bitmap(m.imgParede, x, y, NULL);
 			} else if(m.mapa[i][j] == CAMINHO) {
-				al_draw_bitmap(m.caminho, x, y, 0);
-				al_draw_bitmap(m.caminho, x, y, 0);
-			} else if(m.mapa[i][j] == PERSONAGEM_VAZIO) {
-				al_draw_bitmap(m.caminho, x, y, 0);
-				al_draw_bitmap(f.fomiga, x, y, 0);
-			} else if(m.mapa[i][j] == PERSONAGEM_CHEIO) {
-				al_draw_bitmap(m.caminho, x, y, 0);
-				al_draw_bitmap(f.fomiga, x, y, 0);
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+			} else if(m.mapa[i][j] == ITEM_PA) {
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+			} else if(m.mapa[i][j] == ITEM_BOTA) {
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+			} else if(m.mapa[i][j] == ITEM_TOCHA) {
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+			} else if(m.mapa[i][j] == ITEM_KIT) {
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
 			} else if(m.mapa[i][j] == ARMAZEM_MAPA_1) {
-
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
 			} else if(m.mapa[i][j] == ARMAZEM_MAPA_2) {
-
-			} else if(m.mapa[i][j] == ARMAZEM_MAPA_3) {
-
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+			} else if (m.mapa[i][j] == ARMAZEM_MAPA_3) {
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+			} else if(m.mapa[i][j] == FORMIGA_VAZIO) {
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+				al_draw_bitmap(f.imgFormiga[f.DIRECAO], x, y, NULL);
+			} else if(m.mapa[i][j] == FORMIGA_CHEIO) {
+				al_draw_bitmap(m.imgCaminho, x, y, NULL);
+				al_draw_bitmap(f.imgFormiga[f.DIRECAO], x, y, NULL);
+				al_draw_bitmap(m.imgComida, x, y, NULL);
 			}
 			x += TILE;
 		}
@@ -422,27 +515,52 @@ void imprimir(Mapa m, Controle c, Formiga f) {
 	}
 }
 
-// Verificam se o jogador venceu
-bool venceuDificil(int a[3][4]) {
-	for(size_t i = 0; i < 4; i++)
-		if(a[ARMAZEM_3][i] == SEM_COMIDA)
-			return false;
-	return true;
+// Carrega o modo gráfico
+void carregarGraficos(Controle &c) {
+	al_init();
+	al_set_new_display_flags(ALLEGRO_OPENGL);
+	c.display = al_create_display(LARGURA, ALTURA);
+	al_set_window_title(c.display, "The Ant Quest");
+	al_init_image_addon();
+	al_install_keyboard();
+	c.fila_eventos = al_create_event_queue();
+	al_register_event_source(c.fila_eventos, al_get_keyboard_event_source());
+	al_register_event_source(c.fila_eventos, al_get_display_event_source(c.display));
 }
 
-bool venceuFacil(int a[3][4]) {
-	if(a[ARMAZEM_3][POSICAO_1] == COMIDA_4)
-		return true;
+//Carrega as imagens
+void carregarImagens(Mapa &m, Formiga &f, Item &i) {
+	m.imgParede = al_load_bitmap("PAREDE.png");
+	m.imgCaminho = al_load_bitmap("CAMINHO.png");
+	m.imgBackground = al_load_bitmap("BACKGROUND.png");
+	m.imgPlaca = al_load_bitmap("PLACA.png");
+	m.imgComida = al_load_bitmap("COMIDA.png");
+	m.imgSombra[GRANDE] = al_load_bitmap("SOMBRA_GRANDE.png");
+	m.imgSombra[PEQUENA] = al_load_bitmap("SOMBRA_PEQUENA.png");
+	m.imgArmazem[DESMORONANDO] = al_load_bitmap("ARMAZEM_DESMORONANDO.png");
+	m.imgArmazem[NORMAL] = al_load_bitmap("ARMAZEM.png");
+	m.imgComidaAmazem[COMIDA_1] = al_load_bitmap("COMIDA_1.png");
+	m.imgComidaAmazem[COMIDA_2] = al_load_bitmap("COMIDA_2.png");
+	m.imgComidaAmazem[COMIDA_3] = al_load_bitmap("COMIDA_3.png");
+	m.imgComidaAmazem[COMIDA_4] = al_load_bitmap("COMIDA_4.png");
+	f.imgFormiga[SUBIR] = al_load_bitmap("FORMIGA_CIMA.png");
+	f.imgFormiga[DESCER] = al_load_bitmap("FORMIGA_BAIXO.png");
+	f.imgFormiga[ESQUERDA] = al_load_bitmap("FORMIGA_ESQUERDA.png");
+	f.imgFormiga[DIREITA] = al_load_bitmap("FORMIGA_DIREITA.png");
+}
+
+// Verifica se o jogador venceu
+bool venceu(int a[3][4]) {
+	for(size_t LOCAL = 0; LOCAL < 4; LOCAL++)
+		if(a[ARMAZEM_1][LOCAL] == SEM_COMIDA)
+			return true;
 	return false;
 }
-//-------------------------------
 // Tela de fim de jogo
-bool venceuJogo(double tempo) {
+bool fimJogo(double tempo) {
 	/*
-	system("cls");
 	cout << "VOCE VENCEU O JOGO, PARABENS!!!\n\n";
 	cout << "O tempo total foi: " << tempo << endl << endl;
-	system("pause");
 	*/
 	return true;
 }
@@ -468,11 +586,11 @@ void medirTempo(bool inicio, Mapa &m, Controle &c, Formiga &f) {
 		c.tempoExecucao -= (c.tempoFinal - c.tempoInicial) / (double)CLOCKS_PER_SEC;
 
 		if (c.tempoTotal < 0) {
-
 			novoMapa(m);
-
-			// AQUI VAI TER QUE COLOCAR NO NOVO MAPA A FORMIGA E OS ARMAZÉNS
-
+			posicionarFormiga(m, f);
+			// FALTA CRIAR A FUNÇÃO QUE DISPÕE OS ARMAZÉNS NO MAPA (MATEUS)
+			// FALTA CRIAR A FUNÇÃO QUE DISPÕE OS ITENS NO MAPA (CAIO) 
+			// UM ITEM DE CADA JA TA LEGAL, E LEMBRE-SE QUE ELES PODEM ACUMULAR COM O DO PRÓXIMO MAPA
 			c.tempoTotal = TEMPO_MAXIMO;
 		}
 	}
@@ -493,33 +611,16 @@ int main(void) {
 	Mapa m;
 	Formiga f;
 	Controle c;
-	ALLEGRO_DISPLAY *display = NULL;
+	Item i;
 	/*-------------------------------------------------------------------------*/
 
 	/*------------------------------ INICIALIZAÇÃO ----------------------------*/
 	// Inicializando gráficos
-	al_init();
-	al_set_new_display_flags(ALLEGRO_OPENGL);
-	display = al_create_display(LARGURA, ALTURA);
-	al_set_window_title(display, "The Ant Quest");
-	al_init_image_addon();
-	al_install_keyboard();
-	c.fila_eventos = al_create_event_queue();
-	al_register_event_source(c.fila_eventos, al_get_keyboard_event_source());
-	al_register_event_source(c.fila_eventos, al_get_display_event_source(display));
 
-	// Carregando imagens
-	m.parede = al_load_bitmap("PAREDE.png");
-	m.caminho = al_load_bitmap("CAMINHO.png");
-	m.background = al_load_bitmap("BACKGROUND.png");
-	m.placa = al_load_bitmap("PLACA.png");
-	m.sombraGrande = al_load_bitmap("SOMBRA_GRANDE.png");
-	m.sombraPequena = al_load_bitmap("SOMBRA_PEQUENA.png");
-	f.fomiga = al_load_bitmap("FORMIGA.png");
-
-	// Inicialização dos conteúdos
+	carregarGraficos(c);
+	carregarImagens(m, f, i);
 	novoMapa(m);
-	novaFormiga(m, f);
+	posicionarFormiga(m, f);
 	iniciarArmazem(m.armazem);
 
 	/*------------------------------ LOOP PRINCIPAL ---------------------------*/
@@ -530,23 +631,21 @@ int main(void) {
 			menu(c);
 		} else if (c.jogar) {
 			//medirTempo(true, m, c, f);
-			imprimir(m, c, f);
+			desenharFrame(m, c, f);
 			lerComandos(m, c, f);
 			//medirTempo(false, m, c, f);
 		} else if(c.pausa) {
 
 		}
 
-		if(c.facil && venceuFacil(m.armazem)) {
-			c.sair = venceuJogo(c.tempoExecucao);
-		} else if(!c.facil && venceuDificil(m.armazem)) {
-			c.sair = venceuJogo(c.tempoExecucao);
+		if(venceu(m.armazem)) {
+			c.sair = fimJogo(c.tempoExecucao);
 		}
 
 		al_flip_display();
 	}
 	/*-------------------------------------------------------------------------*/
-	al_destroy_display(display);
+	al_destroy_display(c.display);
 	al_destroy_event_queue(c.fila_eventos);
 	return 0;
 }
